9b9fbbeb114dd0c7ce4f73e6ca94c920b0e22d3c
mrhorst <mhorst31@gmail.com>
2024-11-15 13:29:43 -0500

    still debugging issues in production

--- utils/db.ts
+++ utils/db.ts
@@ -4,22 +4,17 @@ import { createClient } from '@libsql/client'
 
 // Create a Turso client
 const libsql = createClient({
+  // url: 'file:./dev.db',
   url: `${process.env.TURSO_DATABASE_URL}`,
   authToken: `${process.env.TURSO_AUTH_TOKEN}`,
 })
-
-declare global {
-  var prisma: PrismaClient | undefined
-}
 
 // Adapter for Turso
 const adapter = new PrismaLibSQL(libsql)
 
 // Initialize Prisma Client
-const prisma = global.prisma || new PrismaClient({ adapter })
-
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma // Cache Prisma client in development
-}
+const prisma = new PrismaClient({
+  adapter,
+})
 
 export default prisma
